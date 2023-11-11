220c3047280d62623d0b641dafb754b3c04169a2
Matt Thornton <matthew.thornton13@gmail.com>
2023-11-11 10:35:07 +0000

    Some refactoring.

--- src/full_time.rs
+++ src/full_time.rs
@@ -1,7 +1,32 @@
-use crate::{Fixture, FixtureSide, FixtureType, TeamName};
+use crate::{Fixture, FixtureSide, FixtureType};
 use chrono::prelude::*;
 use chrono_tz::GB;
-use scraper::{ElementRef, Selector};
+use reqwest::Error;
+use scraper::{ElementRef, Html, Selector};
+
+#[derive(Clone, Copy)]
+pub struct TeamId(i32);
+
+impl TeamId {
+    pub fn new(x: i32) -> TeamId {
+        TeamId(x)
+    }
+}
+
+impl std::ops::Deref for TeamId {
+    type Target = i32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct TeamName<'a>(pub &'a str);
+
+pub struct Team<'a> {
+    pub id: TeamId,
+    pub name: TeamName<'a>,
+}
 
 fn parse_fixture_type(cell: &ElementRef) -> FixtureType {
     match cell.inner_html().trim().to_lowercase().as_str() {
@@ -59,11 +84,12 @@ fn parse_teams(
     ) {
         (Some(opposition), None) => (FixtureSide::Away, opposition),
         (None, Some(opposition)) => (FixtureSide::Home, opposition),
-        _ => panic!("Team wasn't list as either home or away for this fixture."),
+        _ => panic!("Team wasn't listed as either home or away for this fixture."),
     }
 }
 
-pub fn parse_fixture(row: &Vec<ElementRef>, team_name: &TeamName) -> Fixture {
+fn parse_fixture<'a>(row: impl Iterator<Item = ElementRef<'a>>, team_name: &TeamName) -> Fixture {
+    let row = row.collect::<Vec<_>>();
     match &row[..] {
         [typ, date_time, home_team, _, _, _, away_team, _] => {
             let (fixture_side, opposition) = parse_teams(home_team, away_team, team_name);
@@ -76,4 +102,31 @@ pub fn parse_fixture(row: &Vec<ElementRef>, team_name: &TeamName) -> Fixture {
         }
         _ => panic!("Incorrect number of cells in table row."),
     }
+}
+
+pub async fn get_upcoming_fixtures(team: &Team<'_>) -> Result<Vec<Fixture>, Error> {
+    let html = reqwest::get(format!(
+        "https://fulltime.thefa.com/displayTeam.html?divisionseason=756007599&teamID={}",
+        *team.id
+    ))
+    .await?
+    .text()
+    .await?;
+    let document = Html::parse_document(&html);
+    let tables = document
+        .select(&Selector::parse("div.fixtures-table table").unwrap())
+        .collect::<Vec<_>>();
+    let table = match tables[..] {
+        [t] => t,
+        _ => panic!(
+            "Expected to find one fixture table, but found {}.",
+            tables.len()
+        ),
+    };
+    let td_selector = Selector::parse("td").unwrap();
+    Ok(table
+        .select(&Selector::parse("tbody tr").unwrap())
+        .map(|tr| tr.select(&td_selector))
+        .map(|r| parse_fixture(r, &team.name))
+        .collect::<Vec<_>>())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
-use full_time_spond_sync::{full_time, TeamName};
-use scraper::{Html, Selector};
+use full_time_spond_sync::full_time;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let team_name = TeamName("Twyford Comets FC U7 Stormtroopers");
-    let html = reqwest::get(
-        "https://fulltime.thefa.com/displayTeam.html?divisionseason=756007599&teamID=372755773",
-    )
-    .await?
-    .text()
-    .await?;
-    let document = Html::parse_document(&html);
-    let tables = document
-        .select(&Selector::parse("div.fixtures-table table").unwrap())
-        .collect::<Vec<_>>();
-    let table = match tables[..] {
-        [t] => t,
-        _ => panic!(
-            "Expected to find one fixture table, but found {}.",
-            tables.len()
-        ),
+    let team = full_time::Team {
+        id: full_time::TeamId::new(372755773),
+        name: full_time::TeamName("Twyford Comets FC U7 Stormtroopers"),
     };
-    let rows = table
-        .select(&Selector::parse("tbody tr").unwrap())
-        .map(|tr| {
-            tr.select(&Selector::parse("td").unwrap())
-                .collect::<Vec<_>>()
-        })
-        .map(|r| full_time::parse_fixture(&r, &team_name))
-        .collect::<Vec<_>>();
+    let rows = full_time::get_upcoming_fixtures(&team).await?;
     println!("{:#?}", rows);
     Ok(())
 }
